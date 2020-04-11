e606d7d4f7c15420ac2c16f32e1a2247d4f15423
SparXFusion <s2234nadar@gmail.com>
2020-04-11 11:03:03 +0530

    RMX1831: cleanup RIL blobs

--- RMX1831/RMX1831-vendor.mk
+++ RMX1831/RMX1831-vendor.mk
@@ -7,13 +7,7 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1831/proprietary/etc/init/init.target.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.target.rc \
     vendor/realme/RMX1831/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1831/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
-    vendor/realme/RMX1831/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmtk-ril.so \
-    vendor/realme/RMX1831/proprietary/bin/hw/mtkfusionrild:$(TARGET_COPY_OUT_VENDOR)/bin/hw/mtkfusionrild \    	
-    vendor/realme/RMX1831/proprietary/etc/init/rild.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/rild.rc \ 
-    vendor/realme/RMX1831/proprietary/lib64/libmtkrillog.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmtkrillog.so \
-    vendor/realme/RMX1831/proprietary/lib64/libmtkrilutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmtkrilutils.so \
-    vendor/realme/RMX1831/proprietary/lib64/librilfusion.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librilfusion.so \
-    vendor/realme/RMX1831/proprietary/lib64/libvia-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvia-ril.so 
-
+    vendor/realme/RMX1831/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmtk-ril.so 
+    
 PRODUCT_PACKAGES += \
     Notchsize 
